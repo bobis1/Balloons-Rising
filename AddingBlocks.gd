c6ae6617315ba454e3f4684b000d2447ffc3a153
bobis <josephwaizy@gmail.com>
2025-11-25 22:29:55 -0600

    Added the flicker animation after getting hit. Also added special formations
    The special formations need fixing because they will not fall down.

--- AddingBlocks.gd
+++ AddingBlocks.gd
@@ -2,8 +2,8 @@ extends Node
 var threshold: int
 var specialThreshold: int
 var falling_stuff = preload("res://Obsts/falling_stuff.tscn")
-var flying_stuff = preload("res://Obsts/falling_stuff.tscn")
-var arrow = preload("res://Obsts/Arrow.tscn")
+var flying_stuff = preload("res://Obsts/flying_stuff.tscn")
+var arrow_scene = preload("res://Obsts/Arrow.tscn")
 var rng = RandomNumberGenerator.new()
 var boolOfObject
 
@@ -17,25 +17,34 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Globals.Score > threshold:
+	if Globals.Score >= specialThreshold:
+		#boolOfObject = rng.randi(x) % 2
+		#if boolOfObject == 0:
+		var arrow = arrow_scene.instantiate()
+		arrow.position = Vector2(-322.0, 128.0)
+		add_child(arrow)
+		print("Arrow Spawned")
+		specialThreshold += 20
+		print(get_tree().get_nodes_in_group("DroppedStuff"))
+	if Globals.Score >= threshold && Globals.Score < specialThreshold:
 		boolOfObject = rng.randi() % 2
 		if boolOfObject == 0:
 			var falling = falling_stuff.instantiate()
 			falling.position = Vector2(-311.0, 0.0)
 			add_child(falling)
+			print('falling')
 		if boolOfObject == 1:
 			var flying = flying_stuff.instantiate()
 			flying.position = Vector2(-213.0, 396.0)
 			add_child(flying)
-	if Globals.Score > specialThreshold:
-		boolOfObject = rng.randi() % 2
-		if boolOfObject == 0:
-			var arrow = arrow.instantiate()
-			arrow.position = Vector2(4.0, -159.0)
-			print("Arrow Spawned")
-			
+			print('flying')
 		threshold += 10
-		specialThreshold += 20
 		print("block spawned")
+		print(get_tree().get_nodes_in_group("DroppedStuff"))
+	
+	
+	
+	
+	
 
 	pass
